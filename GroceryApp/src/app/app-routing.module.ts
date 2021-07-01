a7dd65a5eb00a63dded72c60d3bc3f18e2bd5a5c
rkywizz <rkywizz@gmail.com>
2021-07-01 15:24:27 +0530

    New Project

--- GroceryApp/src/app/app-routing.module.ts
+++ GroceryApp/src/app/app-routing.module.ts
@@ -1,15 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './login/login.component';
-import { ProductComponent } from './product/product.component';
-import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './user/login/login.component';
+import { RegisterComponent } from './user/register/register.component';
 
 const routes: Routes = [
   {path:'',redirectTo:'home',pathMatch:'full'},
   {path:'home',component:HomeComponent},
   {path:'login',component:LoginComponent},
-{path:'product',component:ProductComponent},
   {path:'register',component:RegisterComponent}
 ];
 
--- GroceryApp/src/app/app.module.ts
+++ GroceryApp/src/app/app.module.ts
@@ -4,15 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './login/login.component';
 import { MenuBarComponent } from './menu-bar/menu-bar.component';
-import { RegisterComponent } from './register/register.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { FormsModule } from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
-import { ProductComponent } from './product/product.component';
-import { UpdateuserComponent } from './updateuser/updateuser.component';
-import { UserComponent } from './delete/user/user.component';
 import { DeleteComponent } from './user/delete/delete.component';
 import { SearchComponent } from './user/search/search.component';
 import { ForgotpasswordComponent } from './user/forgotpassword/forgotpassword.component';
@@ -23,9 +18,12 @@ import { AddProductComponent } from './product/add-product/add-product.component
 import { UpdateProductComponent } from './product/update-product/update-product.component';
 import { DeleteProductComponent } from './product/delete-product/delete-product.component';
 import { SearchProductComponent } from './product/search-product/search-product.component';
-import { BookOrderComponent } from './oder/book-order/book-order.component';
-import { CancelOrderComponent } from './oder/cancel-order/cancel-order.component';
-import { UpdateOrderComponent } from './oder/update-order/update-order.component';
+import { LoginComponent } from './user/login/login.component';
+import { RegisterComponent } from './user/register/register.component';
+import { UpdateuserComponent } from './user/updateuser/updateuser.component';
+import { BookOrderComponent } from './order/book-order/book-order.component';
+import { CancelOrderComponent } from './order/cancel-order/cancel-order.component';
+import { UpdateOrderComponent } from './order/update-order/update-order.component';
 
 @NgModule({
   declarations: [
@@ -35,9 +33,7 @@ import { UpdateOrderComponent } from './oder/update-order/update-order.component
     MenuBarComponent,
     RegisterComponent,
     PageNotFoundComponent,
-    ProductComponent,
     UpdateuserComponent,
-    UserComponent,
     DeleteComponent,
     SearchComponent,
     ForgotpasswordComponent,
