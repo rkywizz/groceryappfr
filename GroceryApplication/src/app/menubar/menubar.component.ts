22320668c6aa9939ada8415b214cbc9665f51107
saran <rsaranrajrps@gmail.com>
2021-07-07 16:40:42 +0530

    logout

--- GroceryApplication/src/app/menubar/menubar.component.ts
+++ GroceryApplication/src/app/menubar/menubar.component.ts
@@ -6,8 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menubar.component.css']
 })
 export class MenubarComponent implements OnInit {
-
-  constructor() { }
+ 
+ 
+  constructor() {}
 
   ngOnInit(): void {
   }
