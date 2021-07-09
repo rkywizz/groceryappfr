1963e5b4a83148433eaa5875bb9c3adb97e299ed
saran <rsaranrajrps@gmail.com>
2021-07-09 16:25:43 +0530

    stock

--- GroceryApplication/src/app/Product/product-list/product-list.component.ts
+++ GroceryApplication/src/app/Product/product-list/product-list.component.ts
@@ -13,9 +13,10 @@ import { ProductServicesService } from '../productService/product-services.servi
 })
 export class ProductListComponent implements OnInit {
   productList:ProductModel[]=[];
-  
+ 
+  stockStatus:string='order';
 name:string='';
-
+success:boolean=true;
 totalRecords:string='';
 page:number=1
   constructor(private service:ProductServicesService,private router:Router) {
@@ -45,4 +46,26 @@ search(){
   );
    
 }
+checkProduct(quantity:number)
+{
+  if(quantity==0){
+    this.stockStatus="Sold Out!!!"
+    this.success=true;
+    return true;
+  }
+this.stockStatus="Order"
+this.success=false;
+return true;
+
+}
+checkStock(quantity:number){
+  if(quantity==0){
+    
+    return true;
+
+  }
+  return false;
+
+}
+
 }
